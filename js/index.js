5f74ea4462035848504fd338d620d8e48d121832
iferrer20 <iferreriestacio@gmail.com>
2021-10-18 17:28:11 +0200

    VAO Added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-"use strict";
+'use strict';
 
 const canvas = document.getElementById('glcanvas');
+const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+const oes_vao_ext = gl.getExtension('OES_vertex_array_object');
+gl.bindVertexArrayOES = oes_vao_ext.bindVertexArrayOES.bind(oes_vao_ext);
+gl.createVertexArrayOES = oes_vao_ext.createVertexArrayOES.bind(oes_vao_ext);
+
+const timeStart = Date.now()/1000;
 
 // WINDOW
 const Window = {
@@ -25,14 +31,23 @@ const Render = {
   gl: null,
 
   onResizeWindow() {
-    this.gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.viewport(0, 0, canvas.width, canvas.height);
   },
 
   update() {
-    for (let f of this.renderUpdateQueue) {
-      f();
-    }
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT); 
+    for (let object of Objects.objects) {
+      gl.uniform1f(object.timeUniform, (Date.now()/1000)-timeStart);
 
+      gl.bindVertexArrayOES(object.vao);
+      gl.useProgram(object.shaderProgram);
+
+      for (let instance of object.instances) {
+        gl.drawArrays(object.drawType, 0, object.indices.length);
+      }
+      
+    }
     requestAnimationFrame(() => this.update());
   },
 
@@ -41,27 +56,46 @@ const Render = {
   },
 
   init() {
-    // Initialize context
-    try {
-      this.gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    } catch(e) {}
 
-    if (!this.gl) {
-      alert('Gl not suported');
-      return;
-    }
+    Window.addResizeCallback(this.onResizeWindow);
     this.onResizeWindow();
-    
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // gl.enable(gl.DEPTH_TEST);
     // gl.depthFunc(gl.LEQUAL);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT|this.gl.DEPTH_BUFFER_BIT); 
+
 
     this.update();
   }
 }
 
 const Triangle = {
+  drawType: gl.TRIANGLES,
+
+  vShaderCode: `
+  attribute vec3 vPos;
+  attribute vec3 position;
+
+  void main(void) {
+    gl_Position = vec4(vPos + position, 1.0);
+  }
+  `,
+  fShaderCode: `
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+
+  uniform float iTime;
+  void main(void) {
+    gl_FragColor = vec4(cos(iTime)/2.0+0.5, 0.0, 0.0, 1.0);
+  }
+  `,
+  instances: [
+    {
+      attrs: {
+        position: [0, 1, 0] 
+      }
+    }
+  ],
+
   vertices: [
     -0.5,0.5,0.0,
     -0.5,-0.5,0.0,
@@ -81,24 +115,18 @@ const Objects = {
   objects: [
     Triangle
   ],
-  instances: [
-
-  ],
-
-  update() {
-    for (let object of this.objects)
-      object.update();
-  },
 
   init() {
     for (let object of this.objects) {
-      object.init();
 
-      var vertex_buffer;
-      // Prepair Buffers
-      {
-        vertex_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+      { // Prepair Buffers
+
+        var vao = gl.createVertexArrayOES();
+        gl.bindVertexArrayOES(vao);
+        
+        var vBuffer = gl.createBuffer();
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
           
         // Pass the vertex data to the buffer
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(object.vertices), gl.STATIC_DRAW);
@@ -107,10 +135,10 @@ const Objects = {
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
         // Create an empty buffer object to store Index buffer
-        var Index_Buffer = gl.createBuffer();
+        var iBuffer = gl.createBuffer();
 
         // Bind appropriate array buffer to it
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
 
         // Pass the vertex data to the buffer
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(object.indices), gl.STATIC_DRAW);
@@ -118,15 +146,89 @@ const Objects = {
         // Unbind the buffer
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
+//        var posBuffer = gl.createBuffer();
+
+ //       gl.bindBuffer(gl.ARRAY_BUFFER, new Float32Array())
+
       }
+
+      { // Prepair shaders
+
+        // Create a vertex shader object
+        var vShader = gl.createShader(gl.VERTEX_SHADER);
+
+        // Attach vertex shader source code
+        gl.shaderSource(vShader, object.vShaderCode);
+
+        // Compile the vertex shader
+        gl.compileShader(vShader);
+
+        //fragment shader source code
+        // Create fragment shader object
+        var fShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+        // Attach fragment shader source code
+        gl.shaderSource(fShader, object.fShaderCode);
+
+        // Compile the fragmentt shader
+        gl.compileShader(fShader);
+
+        // Create a shader program object to store
+        // the combined shader program
+        var shaderProgram = gl.createProgram();
+
+        // Attach a vertex shader
+        gl.attachShader(shaderProgram, vShader);
+
+        // Attach a fragment shader
+        gl.attachShader(shaderProgram, fShader);
+
+        // Link both the programs
+        gl.linkProgram(shaderProgram);
+
+        // Use the combined shader program object
+        gl.useProgram(shaderProgram);
+      }
+
+      {
+        // Bind vertex buffer object
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+
+        // Bind index buffer object
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+
+        for (let instance of object.instances) {
+
+        }
+
+        // Get the attribute location
+        var coord = gl.getAttribLocation(shaderProgram, 'vPos');
+
+        // Point an attribute to the currently bound VBO
+        gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); 
+
+        // Enable the attribute
+        gl.enableVertexAttribArray(coord); 
+
+      }
+
+      {
+        object.timeUniform = gl.getUniformLocation(shaderProgram, 'iTime');
+        object.shaderProgram = shaderProgram;
+        object.vao = vao;
+      }
+
+      object.init();
     }
     
-      
-    Render.addUpdate(this.update.bind(this));
-
   }
 }
 
-Window.init();
-Render.init();
-Objects.init();
+if (!gl) {
+  alert('webgl not suported');
+} else {
+  Window.init();
+  Render.init();
+  Objects.init();
+}
+
